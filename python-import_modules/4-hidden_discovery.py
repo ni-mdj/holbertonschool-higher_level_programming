96dea89d5203e5d732bfd60ed5cde3012de2e8ac
TonNom <ton-email@example.com>
2025-01-17 22:02:55 +0100

    add problem with pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+import hidden_4
+
 if __name__ == "__main__":
-    import hidden_4
     names = dir(hidden_4)
-    for name in names:
-        if name[:2] != "__":
+    for name in sorted(names):
+        if not name.startswith("__"):
             print(name)
