bd9f8ab1dc19e7fb5d13fc7bd3070c0af33d9307
TonNom <ton-email@example.com>
2025-01-17 21:17:13 +0100

    add task of project 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
+import types
+import sys
+import importlib.util
+
+def get_names_from_pyc(pyc_file):
+    # Load the .pyc file using importlib
+    spec = importlib.util.spec_from_file_location("hidden_4", pyc_file)
+    module = importlib.util.module_from_spec(spec)
+    spec.loader.exec_module(module)
+
+    names = dir(module)
+
+    filtered_names = [name for name in names if not name.startswith('__')]
+
+    filtered_names.sort()
+    
+    return filtered_names
+
 if __name__ == "__main__":
-    import hidden_4 # type: ignore
-    names = dir(hidden_4)
+    pyc_file = '/tmp/hidden_4.pyc'
+    
+    names = get_names_from_pyc(pyc_file)
+
     for name in names:
-        if name[:2] != "__":
-            print(name)
+        print(name)
