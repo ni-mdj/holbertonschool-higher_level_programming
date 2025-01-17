1db0a3f8b8a9db064028e0f90747ce88628e2191
TonNom <ton-email@example.com>
2025-01-17 13:54:16 +0100

    add task 1;2;3;4;5 with rayane

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
-for letter in range(97, 123):
-    if chr(letter) not in ['q', 'e']:
-        print(chr(letter), end="")
+for i in range(97, 123):
+    if i != 113 and i != 101:
+        print("{}".format(chr(i)), end="")
