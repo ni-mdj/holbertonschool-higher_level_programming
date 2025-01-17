34fa98d5dfdf183f43a6917348834f7c195a461b
TonNom <ton-email@example.com>
2025-01-17 21:56:10 +0100

    add problem with pycodestyle

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,26 +1,7 @@
 #!/usr/bin/python3
-import types
-import sys
-import importlib.util
-
-def get_names_from_pyc(pyc_file):
-    # Load the .pyc file using importlib
-    spec = importlib.util.spec_from_file_location("hidden_4", pyc_file)
-    module = importlib.util.module_from_spec(spec)
-    spec.loader.exec_module(module)
-
-    names = dir(module)
-
-    filtered_names = [name for name in names if not name.startswith('__')]
-
-    filtered_names.sort()
-    
-    return filtered_names
-
 if __name__ == "__main__":
-    pyc_file = '/tmp/hidden_4.pyc'
-    
-    names = get_names_from_pyc(pyc_file)
-
+    import hidden_4
+    names = dir(hidden_4)
     for name in names:
-        print(name)
+        if name[:2] != "__":
+            print(name)
