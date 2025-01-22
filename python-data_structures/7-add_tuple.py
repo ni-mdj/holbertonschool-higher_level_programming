20ffe9b46a7d9e2ab5c01a524503ec975ec371e8
TonNom <ton-email@example.com>
2025-01-22 04:59:25 +0100

    add problem with pycodestyle

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
 
-def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
-    return a_dictionary
+def add_tuple(tuple_a=(), tuple_b=()):
+    # Extend tuples to at least 2 elements, filling with 0s if necessary
+    a = tuple_a + (0, 0)
+    b = tuple_b + (0, 0)
+    
+    # Sum the first two elements of each tuple
+    return (a[0] + b[0], a[1] + b[1])
