7f117132e1ecfafa0ec719603918f8769149f875
TonNom <ton-email@example.com>
2025-02-07 22:36:35 +0100

    add test

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,62 +1,20 @@
 #!/usr/bin/python3
-"""Module for BaseGeometry class."""
+"""11-square.py
+"""
 
 
-class BaseGeometry:
-    """A class with attributes and methods for geometry."""
-
-    def area(self):
-        """Not implemented."""
-        raise Exception("area() is not implemented")
-
-    def integer_validator(self, name, value):
-        """Validates a given value."""
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """A class with attributes and methods for a rectangle."""
-
-    def __init__(self, width, height):
-        """Instantiation with width and height."""
-        self.integer_validator("width", width)
-        self.integer_validator("height", height)
-        self.__width = width
-        self.__height = height
-
-    def area(self):
-        """Returns the area of the Rectangle."""
-        return self.__width * self.__height
-
-    def __str__(self):
-        """Returns string representation of rectangle using #."""
-        return "[Rectangle] {}/{}".format(self.__width, self.__height)
-
-    def print(self):
-        """Prints the rectangle using #."""
-        print(str(self))
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """A class with attributes and methods for a square."""
+    """ inherits from Rectangle """
 
     def __init__(self, size):
-        """Instantiation with size."""
+        """ Constructor """
         self.integer_validator("size", size)
         self.__size = size
         super().__init__(size, size)
 
-    def area(self):
-        """Returns the area of the Square."""
-        return self.__size ** 2
-
     def __str__(self):
-        """Returns string representation of square using #."""
-        return "[Square] {}/{}".format(self.__size, self.__size)
-
-    def print(self):
-        """Prints the square using #."""
-        print(str(self))
+        """ print(self) method """
+        return '[Square] {:d}/{:d}'.format(self.__size, self.__size)
