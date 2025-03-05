70702362485dcc3ff3746bf1ad0beaed41161cd5
ni-mdj <9855@holbertonstudents.com>
2025-03-05 03:42:31 +0000

    task 6 complet

---
+++ SQL_introduction/6-list_values.sql
@@ -0,0 +1,5 @@
+-- Lists all rows of the table first_table from the database hbtn_0c_0 in MySQL server.
+-- This script lists all rows of the table first_table from the database hbtn_0c_0 in MySQL server.
+
+SELECT * FROM first_table;
+
