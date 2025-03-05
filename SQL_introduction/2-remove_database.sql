13ca287520ff2fb6fd09191c3a9d666728a65c0f
ni-mdj <9855@holbertonstudents.com>
2025-03-05 03:23:58 +0000

    add task 2

---
+++ SQL_introduction/2-remove_database.sql
@@ -0,0 +1,4 @@
+-- Delete the database hbtn_0c_0 if it exists in MySQL server and do nothing if it does not exist.
+-- This script deletes a database in MySQL server if it exists already and does nothing if it does not exist.
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
