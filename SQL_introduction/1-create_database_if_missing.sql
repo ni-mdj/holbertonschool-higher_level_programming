725eb71d6551b25bcc7455a9c0a8ff026332a9ee
ni-mdj <9855@holbertonstudents.com>
2025-03-05 03:21:17 +0000

    add task 1

---
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -0,0 +1,4 @@
+-- Create a database in MySQL server if it does not exist already
+-- This script creates a database in MySQL server if it does not exist already
+
+CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
